1e05666a1119435384953358bb659efd7568c8f9
Liz Towoju <liztowoju@gmail.com>
2022-11-10 11:54:40 +0100

    fix: target_blank for the a tags

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -8,10 +8,10 @@ export default function Footer(){
     return(
         <footer className="footer">
 
-            <a href="https://twitter.com/helizabethan"><img src={TwitterIcon} alt="twitter-icon" /></a>
-            <a href="https://github.com/Liztowoju"><img src={GithubIcon} alt="github-icon" /></a>
-            <a href="https://www.instagram.com/lizzytowoju/"><img src={InstagramIcon} alt="instagram-icon" /></a>
-            <a href="https://www.facebook.com/helizabethan"><img src={FacebookIcon} alt="facebook-icon" /></a>
+            <a href="https://twitter.com/helizabethan" target="_blank"><img src={TwitterIcon} alt="twitter-icon" /></a>
+            <a href="https://github.com/Liztowoju" target="_blank"><img src={GithubIcon} alt="github-icon" /></a>
+            <a href="https://www.instagram.com/lizzytowoju/" target="_blank"><img src={InstagramIcon} alt="instagram-icon" /></a>
+            <a href="https://www.facebook.com/helizabethan" target="_blank"><img src={FacebookIcon} alt="facebook-icon" /></a>
             
         </footer>
     )
--- src/Components/Info.jsx
+++ src/Components/Info.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Headshot from "../images/headshot-image.png";
+import Headshot from "../images/elizabeth-headshot.jpeg";
 import MailIcon from "../images/email-icon.svg";
 import LinkedinIcon from "../images/linkedin-icon.svg";
 
@@ -9,12 +9,12 @@ export default function Info() {
       <img src={Headshot} alt="headshot-photo" />
       <h1 className="name">Elizabeth Towoju</h1>
       <h4 className="job-role">Frontend Developer</h4>
-      <a href="pluginette.com" className="web-address">liztowoju.com</a>
+      <a href="pluginette.com" className="web-address" target="_blank">liztowoju.com</a>
       <div className="profile-area-contact">
         <a href="mailto:liztowoju@gmail.com" className="email-contact">
           <img src={MailIcon} alt="email-icon" /> <span>Email</span>{" "}
         </a>
-        <a href="https://www.linkedin.com/in/h-elizabeth/" className="linkedin-contact"> 
+        <a href="https://www.linkedin.com/in/h-elizabeth/" className="linkedin-contact" target="_blank"> 
           <img src={LinkedinIcon} alt="linkedin-icon" />
           <span>LinkedIn</span>
         </a>
